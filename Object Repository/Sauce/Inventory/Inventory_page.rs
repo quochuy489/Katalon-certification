501e68cf02e1f0e51a88fcdb937a7dbaa096893f
quochuy489 <quochuylu98@gmail.com>
2024-07-21 19:53:46 +0700

    create TCs

---
+++ Object Repository/Sauce/Inventory/Inventory_page.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Inventory_page</name>
+   <tag></tag>
+   <elementGuidId>99364aac-d4c2-4168-b872-ec887180ef8d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id='inventory_container']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
